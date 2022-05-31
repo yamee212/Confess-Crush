0cd37750f81d5bba25e5308128ed4f4d0a1ca90b
yamee212 <106622923+yamee212@users.noreply.github.com>
2022-06-01 01:49:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
-    titleWeb: "Hello",
-    introTitle: 'Hi crush',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    titleWeb: "DanhchoHuong",
+    introTitle: 'Chao em',
+    introDesc: `anh dung day tu chieu`,
+   
+    btnIntro: '^__^',
+    title: 'Hom nay la 1-6 do Huong',
+    desc: 'co qua cho cau be khong, send nude cung duoc ',
+    btnYes: 'co <3',
+    btnNo: 'Khong nha :3',
+    question: 'ngu ngon, toi di ngu day',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'ngu ngon',
+    mess: 'em dep alm',
+    messDesc: 'toi la thung rac, hay vut noi buon cua ban vao day',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100028351548537' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/iino1911' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
